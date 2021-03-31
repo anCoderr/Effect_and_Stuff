a290338b671bcce7cf624dcacc838dcde9a9219e
anCoderr <deyol.anurag998@gmail.com>
2021-04-01 01:38:15 +0530

    new images added

--- Honeycomb Design File for Catan Map/Hexagon.js
+++ Honeycomb Design File for Catan Map/Hexagon.js
@@ -4,8 +4,8 @@ window.onload = (() => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     context.imageSmoothingEnabled = false;
-    context.strokeStyle = "white";
-
+    context.strokeStyle = "yellow";
+    context.clearRect(0, 0, canvas.width, canvas.height); 
     const factor = 0.866;
     const padding = 5;
     const size = 60;
@@ -21,10 +21,47 @@ window.onload = (() => {
     let imageStone = document.getElementById('stone');
     let imageTree = document.getElementById('tree');
 
-    let pattern;
+    let patternClay = context.createPattern(imageClay, 'repeat-y');
+    let patternHay = context.createPattern(imageHay, 'repeat');
+    let patternPasture = context.createPattern(imagePasture, 'repeat');
+    let patternStone = context.createPattern(imageStone, 'repeat');
+    let patternTree = context.createPattern(imageTree, 'repeat');
 
-    pattern =  context.createPattern(imageStone, 'repeat');
-    context.fillStyle = pattern;
+    
+    context.fillStyle = patternClay;
+    context.beginPath();
+    context.arc(100, 100, 100, 0, Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+    context.fillStyle = patternHay;
+    context.beginPath();
+    context.arc(300, 100, 100, 0, Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+    context.fillStyle = patternHay;
+    context.beginPath();
+    context.arc(100, 300, 100, 0, Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+    context.fillStyle = patternClay;
+    context.beginPath();
+    context.arc(300, 300, 100, 0, Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+    context.fillStyle = patternHay;
+    context.beginPath();
+    context.arc(200, 200, 100, 0, Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
 
     const hexDraw = (centerX, centerY, size) => {
         context.beginPath();
@@ -36,42 +73,42 @@ window.onload = (() => {
         context.lineTo(size/2 + centerX, factor*size + centerY);
         context.closePath();
         context.stroke();
-        //context.fillStyle = pattern;
-        context.fill();
-
+        //context.fill();
+        
     }
+
+    context.drawImage(imageClay, absoluteCenterX - 2*distX - 30, absoluteCenterY + 2*distY - 30, 60, 60)
 
     const col1Draw = () => {
         hexDraw(absoluteCenterX - 2*distX, absoluteCenterY + 2*distY, size);
         hexDraw(absoluteCenterX - 2*distX, absoluteCenterY, size);
         hexDraw(absoluteCenterX - 2*distX, absoluteCenterY - 2*distY, size);
-    }
+    };
     const col2Draw = () => {
         hexDraw(absoluteCenterX - distX, absoluteCenterY + 3*distY, size);
         hexDraw(absoluteCenterX - distX, absoluteCenterY + distY, size);
         hexDraw(absoluteCenterX - distX, absoluteCenterY - distY, size);
         hexDraw(absoluteCenterX - distX, absoluteCenterY - 3*distY, size);
-    }
+    };
     const col3Draw = () => {
         hexDraw(absoluteCenterX, absoluteCenterY + 4*distY, size);
         hexDraw(absoluteCenterX, absoluteCenterY + 2*distY, size);
         hexDraw(absoluteCenterX, absoluteCenterY, size);
         hexDraw(absoluteCenterX, absoluteCenterY - 2*distY, size);
         hexDraw(absoluteCenterX, absoluteCenterY - 4*distY, size);
-    }
+    };
     const col4Draw = () => {
         hexDraw(absoluteCenterX + distX, absoluteCenterY + 3*distY, size);
         hexDraw(absoluteCenterX + distX, absoluteCenterY + distY, size);
         hexDraw(absoluteCenterX + distX, absoluteCenterY - distY, size);
         hexDraw(absoluteCenterX + distX, absoluteCenterY - 3*distY, size);
-    }
+    };
     const col5Draw = () => {
         hexDraw(absoluteCenterX + 2*distX, absoluteCenterY + 2*distY, size);
         hexDraw(absoluteCenterX + 2*distX, absoluteCenterY, size);
         hexDraw(absoluteCenterX + 2*distX, absoluteCenterY - 2*distY, size);
-    }
-
-
+    };
+    
     col1Draw();
     col2Draw();
     col3Draw();
